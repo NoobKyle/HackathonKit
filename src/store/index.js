3a469b4540ca6ea04dd30fcabe8ad162a3ce0411
kyle <kadangotinaishe5@gmail.com>
2021-07-06 12:44:55 +0200

    Small Changes

---
+++ src/store/index.js
@@ -0,0 +1,10 @@
+import { createStore } from "redux";
+import { devToolsEnhancer } from "redux-devtools-extension";
+
+import rootReducer from "./modules/rootReducer";
+
+const enhancer = devToolsEnhancer();
+
+const store = createStore(rootReducer, enhancer);
+
+export default store;
