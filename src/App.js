471a7ce2ac8b645b4fdf5345c8e7eb47357f53ad
kyle <kadangotinaishe5@gmail.com>
2021-07-09 16:01:16 +0200

    Redux Stuff

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
-import { Provider } from "react-redux";
 import './App.css';
 
-import store from "./store"
 import Container from '@material-ui/core/Container';
 
 import Main from './pages/main/main';
 function App() {
   return (
-    <Provider store={store}>
       <Container className="App">
         <Main/>
       </Container>
-    </Provider>
   );
 }
 
