4a02a7ab6cb2f8fe6d30a20199ba30a85dc233ea
kyle <kadangotinaishe5@gmail.com>
2021-07-13 15:04:00 +0200

    Small Changes

---
+++ src/pages/learn/hackathons.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import './learn.css';
+
+import Button from '@material-ui/core/Button';
+
+function Resources() {
+  return (
+      <div className="hackathon">
+        <h4>What is a hackathon?</h4>
+        <p>A hackathon is an event, usually hosted by a tech company or organization, where programmers get together for a short period of time to collaborate on a project. The participants work rapidly and often work without sleep to achieve their task, as the events generally only last 24 hours or take place over a weekend.<br/><br/>
+        Hackathons are often competition-style events where a project must be completed in a short time frame, according to Levent Gurses, founder of Movel, a mobile app design and development company. He explains that participants build prototypes of software applications like web or mobile apps. He says he’s attended dozens of hackathons and even won a few.<br/><br/>
+        One example of a successful hackathon “hack” is the messaging app, GroupMe, which Skype ended up acquiring for more than $50 million, according to Kathryn Moore, outreach specialist with the tech company Toptal.<br/><br/>
+        Apart from the competition aspect, Moore explains that many hackathons also host workshops, guest speakers, and connect participants with seasoned mentors.</p>
+        <br/>
+
+        <h4>What do people do at a hackathon?</h4>
+        <p>“Computer programmers and software designers collaborate and create a solution to an existing problem using technology,” Moore explains. Those participating in a hackathon will work with like-minded individuals to utilize new technologies and hack together tons of code from different sources to achieve the goal, according to Sean Hsieh, co-founder and chief product officer at Flowroute.<br/><br/>
+        Not only do programmers get the satisfaction of creating a useful product during the hackathon, but once the time runs out all participants come together and present their creations to a panel of judges, says Luke Harris-Gallahue, a growth marketer at Hurdlr. The judges then vote and determine the winners, says Harris-Gallahue. Often large companies such as Google, Apple, Microsoft or Bloomberg will sponsor the event and give away some great prizes.<br/><br/>
+        Speaking of prizes, it’s time to talk about the best part of hackathons: the benefits.</p>
+        <br/>
+
+        <h4>The Benefits of participating in a hackathon</h4>
+        <p>For anyone looking to enter the tech field, participating in a hackathon can be a great learning experience and offers a unique opportunity to build a powerful network.<br/><br/>
+        “One of the best things about hackathons is the opportunity to meet new people who care about the issue or technology that you care about,” Gurses says. Whether it’s for project collaboration, finding a mentor or even potential employers, hackathons are a great place to make connections that could pay dividends in the future. Harris-Gallahue says his company frequently recruits new employees from hackathons.<br/><br/>
+        Gurses adds that participating in (or winning) a hackathon is also a great resume-builder, especially for those with limited experience in the tech field. Even if participants don’t win the competition, the amount of knowledge a participant gains in such a short period of time is unmatched.<br/><br/>
+        Another tangible perk of participating in a hackathon is the opportunity to win cash and other prizes. It’s unlikely for participants to go home empty-handed, according to Gurses. He says most hackathons give away things like t-shirts, mugs, bags and even tech gadgets.<br/><br/>
+        For those already working in the field, hackathons are a great event for teams of programmers. “It was a 24-hour event without sleep, but a whole lot of fun and a great team experience,” says John Lyotier, co-founder of Left of the Dot. Since that event, Lyotier’s team of programmers has experienced much better communication and greater appreciation of their colleagues’ skills and abilities.<br/><br/>
+        As you can see, participating in a hackathon is a good idea for any aspiring or emerging programmer, but do you have what it takes?</p>
+        <br/>
+
+        <h4>What do you need to participate in a hackathon</h4>
+        <p>This is an important question, especially for beginners looking to increase their involvement in the programming field. The entry qualifications and experience levels vary depending on the event. So whether you’re just dipping your toes into coding, you’re on your way toward earning a programming degree or you’re a seasoned programming pro – there’s a hackathon out there for you!<br/><br/>
+        Most hackathons try to be as inclusive as possible, welcoming programmers of all experience levels. While there is a competitive aspect, many participants are just there to have fun, socialize and learn. If you’re unable to contribute anything to a team, you can always spectate. But our experts advise at least having some basic programming understanding if you are hoping to contribute and follow along.</p>
+        <br/>
+
+        <hr/>
+      </div>
+  );
+}
+
+export default Resources;
